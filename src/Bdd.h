4538189c95390435f0d12470bfc2a454f07bce59
guodong <gd@tongji.edu.cn>
2023-05-05 12:21:25 -0400

    add default bdd constructor

--- src/Bdd.h
+++ src/Bdd.h
@@ -8,8 +8,14 @@ extern NodeTable* nodeTable;
 
 class Bdd {
  public:
+  Bdd() : root_(nodeTable->falseNode()) {}
   Bdd(Node* root) : root_(root) {};
   ~Bdd();
+
+  Bdd& operator=(Bdd r) {
+    root_ = r.root();
+    return *this;
+  }
 
   Node* root() const {
     return root_;
@@ -17,16 +23,25 @@ class Bdd {
 
   Bdd operator&(const Bdd& r) const {
     return nodeTable->bddAnd(root_, r.root());
-  };
+  }
   Bdd operator|(const Bdd& r) const {
     return nodeTable->bddOr(root_, r.root());
-  };
+  }
   Bdd operator!() const {
     return nodeTable->bddNot(root_);
-  };
+  }
   Bdd operator-(const Bdd& r) const {
     return nodeTable->bddDiff(root_, r.root());
   }
+  bool operator==(const Bdd& r) const {
+    return root_ == r.root();
+  };
+  bool operator!=(const Bdd& r) const {
+    return root_ != r.root();
+  };
+  bool operator<(const Bdd& r) const {
+    return root_ < r.root();
+  };
 
  private:
   Node* root_;
